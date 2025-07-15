97be3b09ca10d1b011bdc1f04eaae39fe69eed92
Gustavocatsan <gustavocatsanapb@gmail.com>
2025-07-15 06:33:20 -0300

    Substitui frontend pelo projeto Vite completo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Video Editor App</h1>
+      <p>Frontend do aplicativo de edição de vídeos.</p>
+    </div>
+  )
+}
+
+export default App
