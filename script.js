a72d861840f123d2474ca0e8f192817755ebb962
Gustavocatsan <gustavocatsanapb@gmail.com>
2025-07-14 23:30:15 -0300

    …

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Video Editor Agent frontend loaded.");
+fetch("http://localhost:5000/")
+  .then(response => response.text())
+  .then(data => {
+    const div = document.createElement("div");
+    div.innerText = data;
+    document.body.appendChild(div);
+  })
+  .catch(error => {
+    console.error("Erro ao conectar com o backend:", error);
+  });
